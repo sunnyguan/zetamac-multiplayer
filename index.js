9f2243754daa742fbb0b35637f007db4f1177957
Sunny Guan <sunny.peng.guan@gmail.com>
2022-02-22 16:35:12 -0600

    fix spectate bug, add github link

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-require('./utils')();
+require("dotenv").config();
+require("./utils")();
 
 const API_URL = process.env.API_URL;
 const fetch = require("node-fetch");
@@ -17,170 +17,177 @@ var online = 0;
 var CAP = 30;
 
 function update_players() {
-    io.emit("new player", {
-        numPlayers: Object.keys(players).length,
-        numOnline: online
-    });
+  io.emit("new player", {
+    numPlayers: Object.keys(players).length,
+    numOnline: online,
+  });
 }
 
 function update_games() {
-    io.emit("update games", {
-        games: games
-    });
+  io.emit("update games", {
+    games: games,
+  });
 }
 
 function game_end(game_data) {
-    fetch(API_URL, {
-        method: "POST",
-        body: JSON.stringify(game_data)
-    }).then(r => r.json()).then(res => {
-        console.log(res);
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(game_data),
+  })
+    .then((r) => r.json())
+    .then((res) => {
+      console.log(res);
     });
 }
 
-server.listen(port, function() {
-    console.log("Server listening at port %d", port);
+server.listen(port, function () {
+  console.log("Server listening at port %d", port);
 });
 
 app.use(express.static(__dirname + "/public"));
 
-io.on("connection", function(socket) {
-    ++online;
+io.on("connection", function (socket) {
+  ++online;
 
-    socket.on("enter", function() {
-        update_players();
-        fetch(API_URL).then(r => r.json()).then(res => {
-            socket.emit("highScore", res);
-        });
+  socket.on("enter", function () {
+    update_players();
+    update_games();
+    fetch(API_URL)
+      .then((r) => r.json())
+      .then((res) => {
+        socket.emit("highScore", res);
+      });
+  });
+
+  socket.on("spectate", function (info) {
+    var id = info.id;
+    if (id in games) {
+      games[id].spectators.push(socket.id);
+    } else {
+      console.error("Game does not exist!");
+    }
+  });
+
+  socket.on("add player", function (name) {
+    players[socket.id] = {
+      id: socket.id,
+      name: name,
+      text: "",
+      question: "",
+      score: "",
+      opponent: "-1",
+    };
+    update_players();
+
+    socket.emit("login", {
+      playerId: socket.id,
+      player: players[socket.id],
+      cap: CAP,
     });
 
-    socket.on("spectate", function(info) {
-        var id = info.id;
-        if (id in games) {
-            games[id].spectators.push(socket.id);
-        } else {
-            console.error("Game does not exist!");
-        }
-    });
+    var done = false;
+    Object.keys(players).forEach((key) => {
+      if (!done && key !== socket.id && players[key].opponent === "-1") {
+        // found a match between player "key" and "socket.id"
+        players[socket.id].opponent = key;
+        players[key].opponent = socket.id;
 
-    socket.on("add player", function(name) {
-        players[socket.id] = {
-            id: socket.id,
-            name: name,
-            text: "",
-            question: "",
-            score: "",
-            opponent: "-1",
+        questions = generate_questions(CAP * 3);
+
+        games[key] = {
+          name1: name,
+          name2: players[key].name,
+          id2: socket.id,
+          spectators: [],
         };
-        update_players();
-
-        socket.emit("login", {
-            playerId: socket.id,
-            player: players[socket.id],
-            cap: CAP
-        });
-
-        var done = false;
-        Object.keys(players).forEach((key) => {
-            if (!done && key !== socket.id && players[key].opponent === "-1") {
-                // found a match between player "key" and "socket.id"
-                players[socket.id].opponent = key;
-                players[key].opponent = socket.id;
-
-                questions = generate_questions(CAP * 3);
-
-                games[key] = {
-                    name1: name,
-                    name2: players[key].name,
-                    id2: socket.id,
-                    spectators: []
-                };
-                update_games();
-
-                socket.broadcast.to(key).emit("match found", {
-                    player: players[socket.id],
-                    opponent: socket.id,
-                    questions: questions
-                });
-
-                socket.emit("match found", {
-                    player: players[key],
-                    opponent: key,
-                    questions: questions
-                });
-
-                var time = CAP + 5;
-                var x = setInterval(function() {
-                    if (time <= 0) {
-                        game_end({
-                            'gameId': `${key}:${socket.id}:${new Date().getTime()}`,
-                            'player1': players[socket.id].name,
-                            'player2': players[key].name,
-                            'score1': parseInt(players[socket.id].score),
-                            'score2': parseInt(players[key].score)
-                        });
-                        clearInterval(x);
-                    }
-                    socket.emit("tick", { time: time });
-                    socket.broadcast.to(key).emit("tick", { time: time });
-
-                    // update to spectators as well
-                    if (key in games) {
-                        games[key].spectators.forEach(spectator => {
-                            socket.broadcast.to(spectator).emit("tick", { time: time });
-                        });
-                    }
-
-                    time--;
-                }, 1000);
-
-                done = true;
-            }
-        });
-    });
-
-    function disconnect() {
-        if (socket.id in games)
-            delete games[socket.id];
-        if (players[socket.id])
-            delete games[players[socket.id].opponent];
         update_games();
 
-        delete players[socket.id];
-        update_players();
+        socket.broadcast.to(key).emit("match found", {
+          player: players[socket.id],
+          opponent: socket.id,
+          questions: questions,
+        });
+
+        socket.emit("match found", {
+          player: players[key],
+          opponent: key,
+          questions: questions,
+        });
+
+        var time = CAP + 5;
+        var x = setInterval(function () {
+          if (time <= 0) {
+            game_end({
+              gameId: `${key}:${socket.id}:${new Date().getTime()}`,
+              player1: players[socket.id].name,
+              player2: players[key].name,
+              score1: parseInt(players[socket.id].score),
+              score2: parseInt(players[key].score),
+            });
+            clearInterval(x);
+          }
+          socket.emit("tick", { time: time });
+          socket.broadcast.to(key).emit("tick", { time: time });
+
+          // update to spectators as well
+          if (key in games) {
+            games[key].spectators.forEach((spectator) => {
+              socket.broadcast.to(spectator).emit("tick", { time: time });
+            });
+          }
+
+          time--;
+        }, 1000);
+
+        done = true;
+      }
+    });
+  });
+
+  function disconnect() {
+    if (socket.id in games) delete games[socket.id];
+    if (players[socket.id]) delete games[players[socket.id].opponent];
+    update_games();
+
+    delete players[socket.id];
+    update_players();
+  }
+
+  socket.on("disconnect", function () {
+    disconnect();
+    --online;
+  });
+
+  socket.on("game end", function () {
+    disconnect();
+  });
+
+  socket.on("update keyboard", function (keyboard) {
+    if (!(socket.id in players)) return;
+    players[socket.id].text = keyboard["text"];
+    players[socket.id].question = keyboard["question"];
+    players[socket.id].score = keyboard["score"];
+
+    socket.emit("update positions", {
+      players: players,
+    });
+
+    socket.broadcast.to(players[socket.id].opponent).emit("update positions", {
+      players: players,
+    });
+
+    if (socket.id in games) {
+      games[socket.id].spectators.forEach((spectator) => {
+        socket.broadcast
+          .to(spectator)
+          .emit("update positions", { players: players });
+      });
+    } else if (players[socket.id].opponent in games) {
+      games[players[socket.id].opponent].spectators.forEach((spectator) => {
+        socket.broadcast
+          .to(spectator)
+          .emit("update positions", { players: players });
+      });
     }
-
-    socket.on("disconnect", function() {
-        disconnect();
-        --online;
-    });
-
-    socket.on("game end", function() {
-        disconnect();
-    });
-
-    socket.on("update keyboard", function(keyboard) {
-        if (!(socket.id in players)) return;
-        players[socket.id].text = keyboard["text"];
-        players[socket.id].question = keyboard["question"];
-        players[socket.id].score = keyboard["score"];
-
-        socket.emit("update positions", {
-            players: players,
-        });
-
-        socket.broadcast.to(players[socket.id].opponent).emit("update positions", {
-            players: players,
-        });
-
-        if (socket.id in games) {
-            games[socket.id].spectators.forEach(spectator => {
-                socket.broadcast.to(spectator).emit("update positions", { players: players });
-            });
-        } else if (players[socket.id].opponent in games) {
-            games[players[socket.id].opponent].spectators.forEach(spectator => {
-                socket.broadcast.to(spectator).emit("update positions", { players: players });
-            });
-        }
-    });
+  });
 });
--- public/game.js
+++ public/game.js
@@ -44,235 +44,230 @@ var list_games = document.getElementById("games");
 var highScore = document.getElementById("high-score");
 
 function digitRead(input) {
-    if (textbox1.readOnly) {
-        console.log("game has not been started yet!");
-    } else {
-        var lower = input.toLowerCase().replaceAll(" ", "");
-        textbox1.value += lower;
-        inputEvent({});
-    }
+  if (textbox1.readOnly) {
+    console.log("game has not been started yet!");
+  } else {
+    var lower = input.toLowerCase().replaceAll(" ", "");
+    textbox1.value += lower;
+    inputEvent({});
+  }
 }
 
 function start_speech() {
-    var commands = {
-        ":digit": {
-            regexp: /^((?:(?:\d) *)+)$/,
-            callback: digitRead,
-        },
-        no: function() {
-            textbox1.value = "";
-        },
-    };
-    // annyang.debug(true);
-    annyang.addCommands(commands);
-    annyang.start();
+  var commands = {
+    ":digit": {
+      regexp: /^((?:(?:\d) *)+)$/,
+      callback: digitRead,
+    },
+    no: function () {
+      textbox1.value = "";
+    },
+  };
+  // annyang.debug(true);
+  annyang.addCommands(commands);
+  annyang.start();
 }
 
 function enable_speech() {
-    if (annyang) {
-        if (speech === 1) {
-            annyang.abort();
-            speech = -1;
-            speech_button.textContent = "Enable Speech";
-        } else {
-            start_speech();
-            speech = 1;
-            speech_button.textContent = "Disable Speech";
-        }
+  if (annyang) {
+    if (speech === 1) {
+      annyang.abort();
+      speech = -1;
+      speech_button.textContent = "Enable Speech";
+    } else {
+      start_speech();
+      speech = 1;
+      speech_button.textContent = "Disable Speech";
     }
-    console.log(speech);
+  }
+  console.log(speech);
 }
 
-addEventListener("keydown", function(event) {
-    if (event.keyCode == 13) {
-        if (!playerName) startConnection();
-    }
+addEventListener("keydown", function (event) {
+  if (event.keyCode == 13) {
+    if (!playerName) startConnection();
+  }
 });
 
 function new_question() {
-    var question = questions[question_id++];
-    question1.textContent = question;
-    keyboard["question"] = question;
-    socket.emit("update keyboard", keyboard);
+  var question = questions[question_id++];
+  question1.textContent = question;
+  keyboard["question"] = question;
+  socket.emit("update keyboard", keyboard);
 }
 
 function check() {
-    var q = question1.textContent;
-    if (!!q) {
-        var ans = eval(q);
-        if (ans === parseInt(textbox1.value)) {
-            textbox1.value = "";
-            score1.textContent = parseInt(score1.textContent) + 1 + "";
-            keyboard["text"] = textbox1.value;
-            keyboard["score"] = score1.textContent;
-            socket.emit("update keyboard", keyboard);
-            new_question();
-        }
+  var q = question1.textContent;
+  if (!!q) {
+    var ans = eval(q);
+    if (ans === parseInt(textbox1.value)) {
+      textbox1.value = "";
+      score1.textContent = parseInt(score1.textContent) + 1 + "";
+      keyboard["text"] = textbox1.value;
+      keyboard["score"] = score1.textContent;
+      socket.emit("update keyboard", keyboard);
+      new_question();
     }
+  }
 }
 
 function inputEvent(e) {
-    if (playerName) {
-        check();
-        keyboard["text"] = textbox1.value;
-        socket.emit("update keyboard", keyboard);
-    }
+  if (playerName) {
+    check();
+    keyboard["text"] = textbox1.value;
+    socket.emit("update keyboard", keyboard);
+  }
 }
 
 addEventListener("input", inputEvent, false);
 
-
 function init_names() {
-    Object.keys(players).forEach((key) => {
-        if (key == playerId) {
-            name1.textContent = players[key].name;
-        } else {
-            name2.textContent = players[key].name;
-        }
-    });
+  Object.keys(players).forEach((key) => {
+    if (key == playerId) {
+      name1.textContent = players[key].name;
+    } else {
+      name2.textContent = players[key].name;
+    }
+  });
 }
 
 // socket events
-socket.on("login", function(data) {
-    playerId = data.playerId;
-    players[playerId] = data.player;
-    cap = data.cap;
-    init_names();
+socket.on("login", function (data) {
+  playerId = data.playerId;
+  players[playerId] = data.player;
+  cap = data.cap;
+  init_names();
 });
 
-
-socket.on("highScore", function(data) {
-    highScore.textContent = `High Score: ${data.score} by ${data.player}`;
+socket.on("highScore", function (data) {
+  highScore.textContent = `High Score: ${data.score} by ${data.player}`;
 });
 
-
-socket.on("match found", function(data) {
-    players[data.player.id] = data.player;
-    opponentId = data.opponent;
-    questions = data.questions;
-    document.getElementById("wait").textContent = " score:";
-    score1.textContent = "0";
-    init_names();
+socket.on("match found", function (data) {
+  players[data.player.id] = data.player;
+  opponentId = data.opponent;
+  questions = data.questions;
+  document.getElementById("wait").textContent = " score:";
+  score1.textContent = "0";
+  init_names();
 });
-
 
 function createElementFromHTML(htmlString) {
-    var div = document.createElement("div");
-    div.innerHTML = htmlString.trim();
-    return div.firstChild;
+  var div = document.createElement("div");
+  div.innerHTML = htmlString.trim();
+  return div.firstChild;
 }
-
 
 function spectate(id, id2) {
-    if (spectating === 0) {
-        socket.emit("spectate", { id: id });
-        spec_id1 = id;
-        spec_id2 = id2;
-        console.log("now spectating " + id + " and " + id2);
-        startSpectating();
-        spectating = 1;
-    }
+  if (spectating === 0) {
+    socket.emit("spectate", { id: id });
+    spec_id1 = id;
+    spec_id2 = id2;
+    console.log("now spectating " + id + " and " + id2);
+    startSpectating();
+    spectating = 1;
+  }
 }
 
-socket.on("update games", function(data) {
-    games = data.games;
-    list_games.innerHTML = "";
-    Object.keys(games).forEach((id) => {
-        var info = games[id];
-        var element = createElementFromHTML(`
-            <div class="bg-blue-200 shadow-md rounded-xl p-2 text-center" onclick="spectate('${id}', '${info.id2}')">
+socket.on("update games", function (data) {
+  console.log("updating games");
+  games = data.games;
+  list_games.innerHTML = "";
+  Object.keys(games).forEach((id) => {
+    var info = games[id];
+    var element = createElementFromHTML(`
+            <div class="bg-blue-200 shadow-md rounded-xl p-2 text-center hover:bg-blue-300 cursor-pointer" onclick="spectate('${id}', '${info.id2}')">
                 ${info.name1} vs ${info.name2}
             </div>
             `);
-        list_games.appendChild(element);
-    });
+    list_games.appendChild(element);
+  });
 });
-
 
 function update_online(count) {
-    numPlayers = count.numPlayers;
-    online = count.numOnline;
-    online_counter.textContent = "Online: " + online + ", in game: " + numPlayers;
+  numPlayers = count.numPlayers;
+  online = count.numOnline;
+  online_counter.textContent = "Online: " + online + ", in game: " + numPlayers;
 }
 
-socket.on("new player", function(data) {
-    update_online(data);
+socket.on("new player", function (data) {
+  update_online(data);
 });
 
-socket.on("tick", function(data) {
-    var time = data.time;
-    if (time >= cap + 2) {
-        banner.textContent = time - cap - 1 + "..";
-    } else if (time == cap + 1) {
-        banner.textContent = "GO!";
-    } else if (time == cap) {
-        banner.textContent = time + "";
-        if (spectating !== 1) textbox1.readOnly = false;
-        new_question();
-    } else if (time <= 0) {
-        var final_score1 = parseInt(score1.textContent);
-        var final_score2 = parseInt(score2.textContent);
-        if (final_score1 < final_score2) {
-            banner.textContent =
-                name2.textContent + " won! (refresh to start new game)";
-        } else if (final_score1 > final_score2) {
-            banner.textContent =
-                name1.textContent + " won! (refresh to start new game)";
-        } else {
-            banner.textContent = "Tied game! (refresh to start new game)";
-        }
-
-        textbox1.readOnly = true;
-        socket.emit("game end");
+socket.on("tick", function (data) {
+  var time = data.time;
+  if (time >= cap + 2) {
+    banner.textContent = time - cap - 1 + "..";
+  } else if (time == cap + 1) {
+    banner.textContent = "GO!";
+  } else if (time == cap) {
+    banner.textContent = time + "";
+    if (spectating !== 1) textbox1.readOnly = false;
+    new_question();
+  } else if (time <= 0) {
+    var final_score1 = parseInt(score1.textContent);
+    var final_score2 = parseInt(score2.textContent);
+    if (final_score1 < final_score2) {
+      banner.textContent =
+        name2.textContent + " won! (refresh to start new game)";
+    } else if (final_score1 > final_score2) {
+      banner.textContent =
+        name1.textContent + " won! (refresh to start new game)";
     } else {
-        banner.textContent = time + "";
+      banner.textContent = "Tied game! (refresh to start new game)";
     }
+
+    textbox1.readOnly = true;
+    socket.emit("game end");
+  } else {
+    banner.textContent = time + "";
+  }
 });
 
-socket.on("update positions", function(data) {
-    players = data.players;
+socket.on("update positions", function (data) {
+  players = data.players;
 
-    document.getElementById("wait").textContent = ":";
-    Object.keys(players).forEach((key) => {
-        if (spectating === 1) {
-            if (key === spec_id1) {
-                name1.textContent = players[spec_id1].name;
-                textbox1.value = players[key].text;
-                question1.textContent = players[key].question;
-                score1.textContent = players[key].score;
-            } else if (key === spec_id2) {
-                name2.textContent = players[spec_id2].name;
-                textbox2.value = players[key].text;
-                question2.textContent = players[key].question;
-                score2.textContent = players[key].score;
-            }
-        } else {
-            if (key === opponentId) {
-                textbox2.value = players[key].text;
-                question2.textContent = players[key].question;
-                score2.textContent = players[key].score;
-            }
-        }
-    });
+  document.getElementById("wait").textContent = ":";
+  Object.keys(players).forEach((key) => {
+    if (spectating === 1) {
+      if (key === spec_id1) {
+        name1.textContent = players[spec_id1].name;
+        textbox1.value = players[key].text;
+        question1.textContent = players[key].question;
+        score1.textContent = players[key].score;
+      } else if (key === spec_id2) {
+        name2.textContent = players[spec_id2].name;
+        textbox2.value = players[key].text;
+        question2.textContent = players[key].question;
+        score2.textContent = players[key].score;
+      }
+    } else {
+      if (key === opponentId) {
+        textbox2.value = players[key].text;
+        question2.textContent = players[key].question;
+        score2.textContent = players[key].score;
+      }
+    }
+  });
 });
 
 /* Start connection */
 
-var startConnection = function() {
-    spectating = -1; // disable spectating
+var startConnection = function () {
+  spectating = -1; // disable spectating
 
-    var startEl = document.getElementById("start");
-    var playerEl = document.getElementById("player");
-    playerName = playerEl.value;
-    if (playerName) {
-        document.getElementById("parent").removeChild(startEl);
-        game.style.display = "block";
-        socket.emit("add player", playerName);
-    }
-};
-
-const startSpectating = function() {
-    const startEl = document.getElementById("start");
+  var startEl = document.getElementById("start");
+  var playerEl = document.getElementById("player");
+  playerName = playerEl.value;
+  if (playerName) {
     document.getElementById("parent").removeChild(startEl);
     game.style.display = "block";
+    socket.emit("add player", playerName);
+  }
+};
+
+const startSpectating = function () {
+  const startEl = document.getElementById("start");
+  document.getElementById("parent").removeChild(startEl);
+  game.style.display = "block";
 };
